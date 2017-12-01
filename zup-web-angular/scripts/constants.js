658bebe5917cdb28d7b9a0c5cb0ad8549753fcee
João Paulo Polles <joaopaulopolles@gmail.com>
2017-12-01 11:35:00 -0200

    Alterações

--- zup-web-angular/scripts/constants.js
+++ zup-web-angular/scripts/constants.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'http://particity.cascavel.pr.gov.br:8282',mapLat:'-24.9580531',mapLng:'-53.4677844',mapZoom:'13'})
+.constant('ENV', {name:'production',apiEndpoint:'http://vc.mcr.pr.gov.br:8282',mapLat:'',mapLng:'',mapZoom:''})
 
 ;
